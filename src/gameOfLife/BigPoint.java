9a8044a0c51ada0501b45249134098512dc77b0b
Tetra Gomez <tetra.m.gomez@gmail.com>
2024-07-19 09:36:36 -0700

    Added BigPoint Class
    The BigPoint class is a replica of the Java Point class, however it does not extend Point2D because the methods that must be implemented from that extension do not translate well to this class, due to narrowing conversions. It will be used in place of Point in order to ensure that cells being very far away from the origin do not cause crashes, however this may cause overhead that could possibly lead to poor performance down the line. Perhaps a setting could be made to switch between the two, with a boundary or toroidal behavior avaliable for the implementation using the Java Point class when cells approach the primitive integer limit.

---
+++ src/gameOfLife/BigPoint.java
@@ -0,0 +1,97 @@
+package gameOfLife;
+
+import java.math.BigInteger;
+
+public class BigPoint
+{
+	private BigInteger x;
+	private BigInteger y;
+
+	public BigPoint()
+	{
+		this(new BigInteger(BigInteger.ZERO.toByteArray()), 
+				new BigInteger(BigInteger.ZERO.toByteArray()));
+	}
+	
+	public BigPoint(final BigPoint p)
+	{
+		this(p.x, p.y);
+	}
+	
+	public BigPoint(
+			final BigInteger x, 
+			final BigInteger y)
+	{
+		this.x = new BigInteger(x.toByteArray());
+		this.y = new BigInteger(y.toByteArray());
+	}
+	
+	public BigInteger getX()
+	{
+		return new BigInteger(this.x.toByteArray());
+	}
+	
+	public BigInteger getY()
+	{
+		return new BigInteger(this.y.toByteArray());
+	}
+	
+	public BigPoint getLocation()
+	{
+		return new BigPoint(x, y);
+	}
+	
+	public void setLocation(
+			final int x,
+			final int y)
+	{
+		this.x = BigInteger.valueOf(x);
+		this.y = BigInteger.valueOf(y);
+	}
+	
+	public void setLocation(
+			final double x,
+			final double y)
+	{
+		this.x = BigInteger.valueOf((int) Math.floor(x + 0.5));
+		this.y = BigInteger.valueOf((int) Math.floor(y + 0.5));
+	}
+	
+	public void setLocation(
+			final BigInteger x,
+			final BigInteger y)
+	{
+		move(x, y);
+	}
+	
+	public void move(
+			final BigInteger dx,
+			final BigInteger dy)
+	{
+		this.x = new BigInteger(x.toByteArray());
+		this.y = new BigInteger(y.toByteArray());
+	}
+	
+	public void translate(
+			final BigInteger dx,
+			final BigInteger dy)
+	{
+		this.x.add(dx);
+		this.y.add(dy);
+	}
+	
+	public boolean equals(Object obj)
+	{
+		if (obj instanceof BigPoint)
+		{
+			BigPoint bp = (BigPoint) obj;
+			return (x.equals(bp.x) && y.equals(bp.y));
+		}
+		return super.equals(obj);
+	}
+	
+	public String toString()
+	{
+		return getClass().getName() + "[x=" + x.toString() + ",y" + y.toString() + "]";
+	}
+}
